bb76c8ad15eb5fe3e7abd5e91d53b25bb5b36056
Laura Bodell <bodelllaura@gmail.com>
2019-05-19 20:18:13 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -23,6 +23,11 @@ export default (state = [], action) => {
       index = state.findIndex(quote => quote.id === action.quoteId);
       quote = state[index];
      
+     return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
      
      case 'DOWNVOTE_QUOTE':
  
