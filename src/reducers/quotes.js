9931aef2b685cedd473bd3cedfb89022841a83cf
Laura Bodell <bodelllaura@gmail.com>
2019-05-19 20:40:15 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,36 +1,26 @@
-
-import uuid from "uuid";
-
-
 export default (state = [], action) => {
-  return state;
-
   let index;
   let quote;
 
-
   switch (action.type) {
-    
-    case "ADD_QUOTE":
-      return [...state, action.quote];
- 
-      
+
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
     case 'REMOVE_QUOTE':
-       return state.filter(quote => quote.id !== action.quoteId);
- 
-   
+      return state.filter(quote => quote.id !== action.quoteId);
+
     case 'UPVOTE_QUOTE':
       index = state.findIndex(quote => quote.id === action.quoteId);
       quote = state[index];
-     
-     return [
+
+      return [
         ...state.slice(0, index),
         Object.assign({}, quote, { votes: quote.votes += 1 }),
         ...state.slice(index + 1)
       ];
-     
-  
-       case 'DOWNVOTE_QUOTE':
+
+    case 'DOWNVOTE_QUOTE':
       index = state.findIndex(quote => quote.id === action.quoteId);
       quote = state[index];
       if (quote.votes > 0) {
@@ -41,8 +31,8 @@ export default (state = [], action) => {
         ];
       }
       return state;
- 
-    default:
+
+    default: 
       return state;
   }
 }
