a3824bf3b94abf9df18e20a387f86a64cb531b1c
Laura Bodell <bodelllaura@gmail.com>
2019-05-19 20:39:04 +0000

    Automatically backed up by Learn

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -3,13 +3,13 @@ import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { addQuote } from '../actions/quotes';
 
+
 class QuoteForm extends Component {
 
   state = {
-   content: '', author: '' 
+    content: '',
+    author: ''
   }
-  
-
 
   handleOnChange = event => {
     const { value, name } = event.target;
@@ -18,9 +18,7 @@ class QuoteForm extends Component {
     });
   }
 
-
-
-handleOnSubmit = event => {
+  handleOnSubmit = event => {
     event.preventDefault();
     const quote = {...this.state, id: uuid() };
     this.props.addQuote(quote);
@@ -29,7 +27,7 @@ handleOnSubmit = event => {
       author: ''
     });
   }
-  
+
   render() {
     return (
       <div className="container">
@@ -37,14 +35,15 @@ handleOnSubmit = event => {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-               <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
+                        name="content"
                         value={this.state.content}
-                         onChange={this.handleOnChange}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -54,6 +53,7 @@ handleOnSubmit = event => {
                       <input
                         className="form-control"
                         type="text"
+                        name="author"
                         value={this.state.author}
                         onChange={this.handleOnChange}
                       />
@@ -74,6 +74,16 @@ handleOnSubmit = event => {
   }
 }
 
-//add arguments to connect as needed
-
 export default connect(null, { addQuote })(QuoteForm);
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
